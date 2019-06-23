84a30407569121c3c77d8a0af739f69844b506d0
Ollie-ODea <51890734+Ollie-ODea@users.noreply.github.com>
2019-06-24 09:23:16 +1200

    Loading Code

---
+++ Box/BoxGame/BoxGame/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace BoxGame
+{
+    partial class BoxGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Screen = new System.Windows.Forms.Panel();
+            this.Pad = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.TmrPlayer = new System.Windows.Forms.Timer(this.components);
+            this.Screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Screen
+            // 
+            this.Screen.Controls.Add(this.Pad);
+            this.Screen.Controls.Add(this.Player);
+            this.Screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Screen.Location = new System.Drawing.Point(0, 0);
+            this.Screen.Name = "Screen";
+            this.Screen.Size = new System.Drawing.Size(584, 561);
+            this.Screen.TabIndex = 0;
+            // 
+            // Pad
+            // 
+            this.Pad.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Pad.Location = new System.Drawing.Point(170, 352);
+            this.Pad.Name = "Pad";
+            this.Pad.Size = new System.Drawing.Size(147, 96);
+            this.Pad.TabIndex = 1;
+            this.Pad.TabStop = false;
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Black;
+            this.Player.Location = new System.Drawing.Point(407, 341);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(25, 25);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // TmrPlayer
+            // 
+            this.TmrPlayer.Enabled = true;
+            this.TmrPlayer.Interval = 1;
+            this.TmrPlayer.Tick += new System.EventHandler(this.TmrPlayer_Tick);
+            // 
+            // BoxGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.Screen);
+            this.Name = "BoxGame";
+            this.Text = "BoxGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BoxGame_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.BoxGame_KeyUp);
+            this.Screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Pad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Screen;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer TmrPlayer;
+        private System.Windows.Forms.PictureBox Pad;
+    }
+}
+
---
+++ Box/BoxGame/BoxGame/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoxGame
+{
+    public partial class BoxGame : Form
+    {
+
+        bool left, right;
+        bool jump;
+        int g = 30;
+        int Force;
+
+
+        public BoxGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TmrPlayer_Tick(object sender, EventArgs e)
+        {
+           // 1. side collision of object
+           if (Player.Right > Pad.Left && Player.Left < Pad.Right - Player.Width / 2 && Player.Bottom > Pad.Top)
+            {
+                right = false;
+            }
+
+           if (Player.Left < Pad.Right && Player.Right > Pad.Left - Player.Width)
+            {
+                left = false;
+            }
+
+           ////
+           //2.
+            if (right == true) { Player.Left += 5; }
+            if (left == true) { Player.Left -= 5; }
+
+           if (jump == true)
+            {
+                //falling when player jumps
+                Player.Top -= Force;
+                Force -= 1;
+            }
+
+           if (Player.Top + Player.Height >= Screen.Height)
+            {
+                Player.Top = Screen.Height - Player.Height; //Stop falling whne a bottom
+                jump = false;
+            }
+           else
+            {
+                Player.Top += 5; //falling speed
+            }
+
+           ////
+           //3.
+           //Top collision
+           if (Player.Left + Player.Width - 5 > Pad.Left && Player.Left + Player.Width + 5 < Pad.Left + Pad.Width + Player.Width && Player.Top + Player.Height >= Pad.Top && Player.Top < Pad.Top)
+            {
+                Player.Top = Screen.Height - Pad.Height - Player.Height;
+                Force = 0;
+                jump = false;
+            }
+
+
+        }
+
+
+        private void BoxGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left) { left = true; }
+            if (e.KeyCode == Keys.Escape) { this.Close(); } //Escape = Exit
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    Force = g;
+                }
+            }
+        }
+
+        private void BoxGame_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+        }
+    }
+}
