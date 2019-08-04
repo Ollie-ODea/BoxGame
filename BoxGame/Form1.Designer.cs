2248b612e73de09d9521f2f5b75318f9bc88a2c8
Ollie-ODea <51890734+Ollie-ODea@users.noreply.github.com>
2019-08-05 09:18:11 +1200

    Add files via upload

---
+++ BoxGame/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace BoxGame
+{
+    partial class BoxGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Screen = new System.Windows.Forms.Panel();
+            this.Pad6 = new System.Windows.Forms.PictureBox();
+            this.Pad5 = new System.Windows.Forms.PictureBox();
+            this.Pad4 = new System.Windows.Forms.PictureBox();
+            this.Coin1 = new System.Windows.Forms.PictureBox();
+            this.Pad3 = new System.Windows.Forms.PictureBox();
+            this.Pad2 = new System.Windows.Forms.PictureBox();
+            this.Pad1 = new System.Windows.Forms.PictureBox();
+            this.Enemy = new System.Windows.Forms.PictureBox();
+            this.Pad = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.TmrPlayer = new System.Windows.Forms.Timer(this.components);
+            this.TrmEnemy = new System.Windows.Forms.Timer(this.components);
+            this.TrmCoin = new System.Windows.Forms.Timer(this.components);
+            this.TrmEnemySpd = new System.Windows.Forms.Timer(this.components);
+            this.LblScore = new System.Windows.Forms.Label();
+            this.TrmCollect = new System.Windows.Forms.Timer(this.components);
+            this.Screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Screen
+            // 
+            this.Screen.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Screen.Controls.Add(this.LblScore);
+            this.Screen.Controls.Add(this.Pad6);
+            this.Screen.Controls.Add(this.Pad5);
+            this.Screen.Controls.Add(this.Pad4);
+            this.Screen.Controls.Add(this.Coin1);
+            this.Screen.Controls.Add(this.Pad3);
+            this.Screen.Controls.Add(this.Pad2);
+            this.Screen.Controls.Add(this.Pad1);
+            this.Screen.Controls.Add(this.Enemy);
+            this.Screen.Controls.Add(this.Pad);
+            this.Screen.Controls.Add(this.Player);
+            this.Screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Screen.Location = new System.Drawing.Point(0, 0);
+            this.Screen.Name = "Screen";
+            this.Screen.Size = new System.Drawing.Size(1124, 961);
+            this.Screen.TabIndex = 0;
+            this.Screen.Paint += new System.Windows.Forms.PaintEventHandler(this.Screen_Paint);
+            // 
+            // Pad6
+            // 
+            this.Pad6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Pad6.Location = new System.Drawing.Point(385, 844);
+            this.Pad6.Name = "Pad6";
+            this.Pad6.Size = new System.Drawing.Size(346, 48);
+            this.Pad6.TabIndex = 9;
+            this.Pad6.TabStop = false;
+            // 
+            // Pad5
+            // 
+            this.Pad5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Pad5.Location = new System.Drawing.Point(42, 638);
+            this.Pad5.Name = "Pad5";
+            this.Pad5.Size = new System.Drawing.Size(318, 48);
+            this.Pad5.TabIndex = 8;
+            this.Pad5.TabStop = false;
+            // 
+            // Pad4
+            // 
+            this.Pad4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Pad4.Location = new System.Drawing.Point(751, 638);
+            this.Pad4.Name = "Pad4";
+            this.Pad4.Size = new System.Drawing.Size(331, 48);
+            this.Pad4.TabIndex = 7;
+            this.Pad4.TabStop = false;
+            // 
+            // Coin1
+            // 
+            this.Coin1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Coin1.Location = new System.Drawing.Point(540, 415);
+            this.Coin1.Name = "Coin1";
+            this.Coin1.Size = new System.Drawing.Size(17, 29);
+            this.Coin1.TabIndex = 6;
+            this.Coin1.TabStop = false;
+            this.Coin1.Click += new System.EventHandler(this.Coin1_Click);
+            // 
+            // Pad3
+            // 
+            this.Pad3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Pad3.Location = new System.Drawing.Point(385, 128);
+            this.Pad3.Name = "Pad3";
+            this.Pad3.Size = new System.Drawing.Size(346, 48);
+            this.Pad3.TabIndex = 5;
+            this.Pad3.TabStop = false;
+            // 
+            // Pad2
+            // 
+            this.Pad2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Pad2.Location = new System.Drawing.Point(42, 299);
+            this.Pad2.Name = "Pad2";
+            this.Pad2.Size = new System.Drawing.Size(331, 48);
+            this.Pad2.TabIndex = 4;
+            this.Pad2.TabStop = false;
+            // 
+            // Pad1
+            // 
+            this.Pad1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Pad1.Location = new System.Drawing.Point(751, 299);
+            this.Pad1.Name = "Pad1";
+            this.Pad1.Size = new System.Drawing.Size(331, 48);
+            this.Pad1.TabIndex = 3;
+            this.Pad1.TabStop = false;
+            // 
+            // Enemy
+            // 
+            this.Enemy.BackColor = System.Drawing.Color.Red;
+            this.Enemy.Location = new System.Drawing.Point(33, 49);
+            this.Enemy.Name = "Enemy";
+            this.Enemy.Size = new System.Drawing.Size(25, 25);
+            this.Enemy.TabIndex = 2;
+            this.Enemy.TabStop = false;
+            // 
+            // Pad
+            // 
+            this.Pad.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Pad.Location = new System.Drawing.Point(385, 468);
+            this.Pad.Name = "Pad";
+            this.Pad.Size = new System.Drawing.Size(346, 48);
+            this.Pad.TabIndex = 1;
+            this.Pad.TabStop = false;
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Aqua;
+            this.Player.Location = new System.Drawing.Point(475, 341);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(30, 30);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // TmrPlayer
+            // 
+            this.TmrPlayer.Enabled = true;
+            this.TmrPlayer.Interval = 1;
+            this.TmrPlayer.Tick += new System.EventHandler(this.TmrPlayer_Tick);
+            // 
+            // TrmEnemy
+            // 
+            this.TrmEnemy.Enabled = true;
+            this.TrmEnemy.Interval = 10;
+            this.TrmEnemy.Tick += new System.EventHandler(this.TrmEnemy_Tick);
+            // 
+            // TrmCoin
+            // 
+            this.TrmCoin.Enabled = true;
+            this.TrmCoin.Interval = 1;
+            this.TrmCoin.Tick += new System.EventHandler(this.TrmCoin_Tick);
+            // 
+            // TrmEnemySpd
+            // 
+            this.TrmEnemySpd.Interval = 1;
+            this.TrmEnemySpd.Tick += new System.EventHandler(this.TrmEnemySpd_Tick);
+            // 
+            // LblScore
+            // 
+            this.LblScore.AutoSize = true;
+            this.LblScore.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.LblScore.Location = new System.Drawing.Point(95, 315);
+            this.LblScore.Name = "LblScore";
+            this.LblScore.Size = new System.Drawing.Size(0, 13);
+            this.LblScore.TabIndex = 10;
+            // 
+            // TrmCollect
+            // 
+            this.TrmCollect.Interval = 1;
+            this.TrmCollect.Tick += new System.EventHandler(this.TrmCollect_Tick);
+            // 
+            // BoxGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 961);
+            this.Controls.Add(this.Screen);
+            this.Name = "BoxGame";
+            this.Text = "BoxGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BoxGame_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.BoxGame_KeyUp);
+            this.Screen.ResumeLayout(false);
+            this.Screen.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Screen;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer TmrPlayer;
+        private System.Windows.Forms.PictureBox Pad;
+        private System.Windows.Forms.PictureBox Enemy;
+        private System.Windows.Forms.Timer TrmEnemy;
+        private System.Windows.Forms.PictureBox Pad3;
+        private System.Windows.Forms.PictureBox Pad2;
+        private System.Windows.Forms.PictureBox Pad1;
+        private System.Windows.Forms.PictureBox Coin;
+        private System.Windows.Forms.PictureBox Coin1;
+        private System.Windows.Forms.Timer TrmCoin;
+        private System.Windows.Forms.PictureBox Pad6;
+        private System.Windows.Forms.PictureBox Pad5;
+        private System.Windows.Forms.PictureBox Pad4;
+        private System.Windows.Forms.Timer TrmEnemySpd;
+        private System.Windows.Forms.Label LblScore;
+        private System.Windows.Forms.Timer TrmCollect;
+    }
+}
+
---
+++ BoxGame/Form1.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoxGame
+{
+    public partial class BoxGame : Form
+    {
+
+        
+        
+        bool left, right;
+        bool jump;
+        int g = 30;
+        int Force;
+
+        bool spawncoin;
+        Random rand = new Random();
+        Random Spawn = new Random();
+        int Score = 0;
+
+
+        Random hit = new Random();
+
+        //speeds
+        int EnemySpdud = 1;
+        int EnemySpdlr = 1;
+
+        int PlayerSpd = 8;
+        int FallSpd = 1;
+        
+
+        public BoxGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TmrPlayer_Tick(object sender, EventArgs e)
+        {
+            // 1. side collision of pad
+            if (Player.Right > Pad.Left && Player.Left < Pad.Right - Player.Width && Player.Bottom < Pad.Bottom && Player.Bottom > Pad.Top)
+            {
+                right = false;
+            }
+
+            if (Player.Left < Pad.Right && Player.Right > Pad.Left - Player.Width && Player.Bottom < Pad.Bottom && Player.Bottom > Pad.Top)
+            {
+                left = false;
+            }
+
+
+            // side collision of pad1
+            if (Player.Right > Pad1.Left && Player.Left < Pad1.Right - Player.Width && Player.Bottom < Pad1.Bottom && Player.Bottom > Pad1.Top)
+            {
+                right = false;
+            }
+
+            if (Player.Left < Pad1.Right && Player.Right > Pad1.Left - Player.Width && Player.Bottom < Pad1.Bottom && Player.Bottom > Pad1.Top)
+            {
+                left = false;
+            }
+
+
+            // side collision of pad2
+            if (Player.Right > Pad2.Left && Player.Left < Pad2.Right - Player.Width && Player.Bottom < Pad2.Bottom && Player.Bottom > Pad2.Top)
+            {
+                right = false;
+            }
+
+            if (Player.Left < Pad2.Right && Player.Right > Pad2.Left - Player.Width && Player.Bottom < Pad2.Bottom && Player.Bottom > Pad2.Top)
+            {
+                left = false;
+            }
+
+            // side collision of pad3
+            if (Player.Right > Pad3.Left && Player.Left < Pad3.Right - Player.Width && Player.Bottom < Pad3.Bottom && Player.Bottom > Pad3.Top)
+            {
+                right = false;
+            }
+
+            if (Player.Left < Pad3.Right && Player.Right > Pad3.Left - Player.Width && Player.Bottom < Pad3.Bottom && Player.Bottom > Pad3.Top)
+            {
+                left = false;
+            }
+
+            // side collision of pad4
+            if (Player.Right > Pad4.Left && Player.Left < Pad4.Right - Player.Width && Player.Bottom < Pad4.Bottom && Player.Bottom > Pad4.Top)
+            {
+                right = false;
+            }
+
+            if (Player.Left < Pad4.Right && Player.Right > Pad4.Left - Player.Width && Player.Bottom < Pad4.Bottom && Player.Bottom > Pad4.Top)
+            {
+                left = false;
+            }
+
+            // side collision of pad5
+            if (Player.Right > Pad5.Left && Player.Left < Pad5.Right - Player.Width && Player.Bottom < Pad5.Bottom && Player.Bottom > Pad5.Top)
+            {
+                right = false;
+            }
+
+            if (Player.Left < Pad5.Right && Player.Right > Pad5.Left - Player.Width && Player.Bottom < Pad5.Bottom && Player.Bottom > Pad5.Top)
+            {
+                left = false;
+            }
+
+            // side collision of pad6
+            if (Player.Right > Pad6.Left && Player.Left < Pad6.Right - Player.Width && Player.Bottom < Pad6.Bottom && Player.Bottom > Pad6.Top)
+            {
+                right = false;
+            }
+
+            if (Player.Left < Pad6.Right && Player.Right > Pad6.Left - Player.Width && Player.Bottom < Pad6.Bottom && Player.Bottom > Pad6.Top)
+            {
+                left = false;
+            }
+
+
+
+
+
+
+
+
+            //Falling/movement on pads
+            if (right == true) { Player.Left += PlayerSpd; }
+            if (left == true) { Player.Left -= PlayerSpd; }
+
+            if (jump == true)
+            {
+                //falling when player jumps
+                Player.Top -= Force;
+                Force -= FallSpd;
+            }
+
+
+            if (Player.Top + Player.Height >= Screen.Height)
+            {
+                Player.Top = Screen.Height - Player.Height; //Stop falling when at bottom
+                jump = false;
+
+
+            }
+            else
+            {
+                Player.Top += 10; //falling speed
+            }
+
+
+
+
+
+
+
+
+            //top and bottom collision of Pad
+            if (Player.Left + Player.Width > Pad.Left && Player.Left + Player.Width < Pad.Left + Pad.Width + Player.Width && Player.Top + Player.Height >= Pad.Top && Player.Top < Pad.Top)
+            {
+                Player.Top = Pad.Location.Y - Player.Height;
+                Force = 0;
+                jump = false;
+
+            }
+            //head collision
+            if (Player.Left + Player.Width > Pad.Left && Player.Left + Player.Width < Pad.Left + Pad.Width + Player.Width && Player.Top - Pad.Bottom <= 10 && Player.Top - Pad.Top > -10)
+            {
+                Force = -1;
+            }
+
+
+            // top collision of pad1
+            if (Player.Left + Player.Width > Pad1.Left && Player.Left + Player.Width < Pad1.Left + Pad1.Width + Player.Width && Player.Top + Player.Height >= Pad1.Top && Player.Top < Pad1.Top)
+            {
+                Player.Top = Pad1.Location.Y - Player.Height;
+                Force = 0;
+                jump = false;
+
+            }
+            //head collision
+            if (Player.Left + Player.Width > Pad1.Left && Player.Left + Player.Width < Pad1.Left + Pad1.Width + Player.Width && Player.Top - Pad1.Bottom <= 10 && Player.Top - Pad1.Top > -10)
+            {
+                Force = -1;
+            }
+
+            // top collision of pad2
+            if (Player.Left + Player.Width > Pad2.Left && Player.Left + Player.Width < Pad2.Left + Pad2.Width + Player.Width && Player.Top + Player.Height >= Pad2.Top && Player.Top < Pad2.Top)
+            {
+                Player.Top = Pad2.Location.Y - Player.Height;
+                Force = 0;
+                jump = false;
+
+            }
+            //head collision
+            if (Player.Left + Player.Width > Pad2.Left && Player.Left + Player.Width < Pad2.Left + Pad2.Width + Player.Width && Player.Top - Pad2.Bottom <= 10 && Player.Top - Pad2.Top > -10)
+            {
+                Force = -1;
+            }
+
+            // top collision of pad3
+            if (Player.Left + Player.Width > Pad3.Left && Player.Left + Player.Width < Pad3.Left + Pad3.Width + Player.Width && Player.Top + Player.Height >= Pad3.Top && Player.Top < Pad3.Top)
+            {
+                Player.Top = Pad3.Location.Y - Player.Height;
+                Force = 0;
+                jump = false;
+
+            }
+            //head collision
+            if (Player.Left + Player.Width > Pad3.Left && Player.Left + Player.Width < Pad3.Left + Pad3.Width + Player.Width && Player.Top - Pad3.Bottom <= 10 && Player.Top - Pad3.Top > -10)
+            {
+                Force = -1;
+            }
+
+            // top collision of pad4
+            if (Player.Left + Player.Width > Pad4.Left && Player.Left + Player.Width < Pad4.Left + Pad4.Width + Player.Width && Player.Top + Player.Height >= Pad4.Top && Player.Top < Pad4.Top)
+            {
+                Player.Top = Pad4
+                    .Location.Y - Player.Height;
+                Force = 0;
+                jump = false;
+
+            }
+            //head collision
+            if (Player.Left + Player.Width > Pad4.Left && Player.Left + Player.Width < Pad4.Left + Pad4.Width + Player.Width && Player.Top - Pad4.Bottom <= 10 && Player.Top - Pad4.Top > -10)
+            {
+                Force = -1;
+            }
+
+            // top collision of pad5
+            if (Player.Left + Player.Width > Pad5.Left && Player.Left + Player.Width < Pad5.Left + Pad5.Width + Player.Width && Player.Top + Player.Height >= Pad5.Top && Player.Top < Pad5.Top)
+            {
+                Player.Top = Pad5
+                    .Location.Y - Player.Height;
+                Force = 0;
+                jump = false;
+
+            }
+            //head collision
+            if (Player.Left + Player.Width > Pad5.Left && Player.Left + Player.Width < Pad5.Left + Pad5.Width + Player.Width && Player.Top - Pad5.Bottom <= 10 && Player.Top - Pad5.Top > -10)
+            {
+                Force = -1;
+            }
+
+            // top collision of pad6
+            if (Player.Left + Player.Width > Pad6.Left && Player.Left + Player.Width < Pad6.Left + Pad6.Width + Player.Width && Player.Top + Player.Height >= Pad6.Top && Player.Top < Pad6.Top)
+            {
+                Player.Top = Pad6
+                    .Location.Y - Player.Height;
+                Force = 0;
+                jump = false;
+
+            }
+            //head collision
+            if (Player.Left + Player.Width > Pad6.Left && Player.Left + Player.Width < Pad6.Left + Pad6.Width + Player.Width && Player.Top - Pad6.Bottom <= 10 && Player.Top - Pad6.Top > -10)
+            {
+                Force = -1;
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+        
+
+
+        private void BoxGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left) { left = true; }
+            if (e.KeyCode == Keys.Escape) { this.Close(); } //Escape = Exit
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    Force = g;
+                }
+            }
+           
+
+        }
+
+        private void TrmEnemy_Tick(object sender, EventArgs e)
+        {
+            if (Enemy.Left + Enemy.Width > Player.Left)
+            {
+                Enemy.Left -= EnemySpdlr;
+            }
+            if (Enemy.Left + Enemy.Width < Player.Left)
+            {
+                Enemy.Left += EnemySpdlr;
+            }
+            if (Enemy.Top + Enemy.Height > Player.Top)
+            {
+                Enemy.Top -= EnemySpdud;
+            }
+            if (Enemy.Top + Enemy.Height < Player.Top)
+            {
+                Enemy.Top += EnemySpdud;
+            }
+             if (Enemy.Bounds.IntersectsWith(Player.Bounds))
+            {
+                Player.Top = hit.Next(10, 951);
+                Player.Left = hit.Next(10, 1114);
+            }
+
+
+
+        }
+
+        private void Screen_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void TrmCoin_Tick(object sender, EventArgs e)
+        {
+            //Coin Collect
+            if (Player.Bounds.IntersectsWith(Coin1.Bounds))
+            {
+                spawncoin = true;
+
+            }
+
+            if (spawncoin == true)
+            {
+                Score += 1;
+                LblScore.Text = Score.ToString();
+            }
+
+
+            if (spawncoin == true)
+            {
+                if (Spawn.Next(1, 7) == 1)
+                {
+                    Coin1.Left = 535;
+                    Coin1.Top = 73;
+                    spawncoin = false;
+                }
+                if (Spawn.Next(1, 7) == 2)
+                {
+                    Coin1.Left = 908;
+                    Coin1.Top = 254;
+                    spawncoin = false;
+                }
+                if (Spawn.Next(1, 7) == 3)
+                {
+                    Coin1.Left = 195;
+                    Coin1.Top = 254;
+                    spawncoin = false;
+                }
+                if (Spawn.Next(1, 7) == 4)
+                {
+                    Coin1.Left = 535;
+                    Coin1.Top = 423;
+                    spawncoin = false;
+                }
+                if (Spawn.Next(1, 7) == 5)
+                {
+                    Coin1.Left = 195;
+                    Coin1.Top = 582;
+                    spawncoin = false;
+                }
+                if (Spawn.Next(1, 7) == 6)
+                {
+                    Coin1.Left = 908;
+                    Coin1.Top = 582;
+                    spawncoin = false;
+                }
+                if (Spawn.Next(1, 7) == 7)
+                {
+                    Coin1.Left = 535;
+                    Coin1.Top = 797;
+                    spawncoin = false;
+                }
+
+            }
+        }
+
+        private void Coin1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrmEnemySpd_Tick(object sender, EventArgs e)
+        {
+            if (Score == 10)
+            {
+                EnemySpdud = 7;
+                EnemySpdlr = 7;
+            }
+        }
+
+        private void TrmCollect_Tick(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void BoxGame_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+        }
+    }
+}
